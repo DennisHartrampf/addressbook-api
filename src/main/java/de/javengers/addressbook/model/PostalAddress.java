66f095a76fa6abbd4f771a8f48ee7cae76e471f8
marat.timergaliev@postcon.de <marat.timergaliev@postcon.de>
2020-10-06 13:07:05 +0200

    missing id field in post address

--- src/main/java/de/javengers/addressbook/model/PostalAddress.java
+++ src/main/java/de/javengers/addressbook/model/PostalAddress.java
@@ -1,6 +1,7 @@
 package de.javengers.addressbook.model;
 
 public class PostalAddress {
+    private Long id;
     private String street;
     private String houseNumber;
     private String zipCode;
@@ -45,5 +46,13 @@ public class PostalAddress {
 
     public String getCountry() {
         return country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/test/java/de/javengers/addressbook/model/PostalAddressTest.java
+++ src/test/java/de/javengers/addressbook/model/PostalAddressTest.java
@@ -9,13 +9,15 @@ class PostalAddressTest {
     @Test
     void testGettersAndSetters() {
         PostalAddress postalAddress = new PostalAddress();
-        
+
+        postalAddress.setId(1L);
         postalAddress.setStreet("Street");
         postalAddress.setHouseNumber("HouseNumber");
         postalAddress.setZipCode("ZipCode");
         postalAddress.setCity("City");
         postalAddress.setCountry("Country");
-        
+
+        assertThat(postalAddress.getId()).isEqualTo(1);
         assertThat(postalAddress.getStreet()).isEqualTo("Street");
         assertThat(postalAddress.getHouseNumber()).isEqualTo("HouseNumber");
         assertThat(postalAddress.getZipCode()).isEqualTo("ZipCode");
