1c159360d574a0314a799fdbb263a80095751657
birqan <birkan.genc@gmail.com>
2020-10-06 12:54:56 +0200

    AA-12 part 1

---
+++ src/main/java/de/javengers/addressbook/model/User.java
@@ -0,0 +1,16 @@
+package de.javengers.addressbook.model;
+
+public class User {
+
+    private Long id;
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+}
