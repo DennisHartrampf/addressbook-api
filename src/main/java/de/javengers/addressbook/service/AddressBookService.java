e873c85deadd4e3487bf31a82914dbdac553dc44
marat.timergaliev@postcon.de <marat.timergaliev@postcon.de>
2020-10-07 13:21:05 +0200

    addressbook service part

--- src/main/java/de/javengers/addressbook/service/AddressBookService.java
+++ src/main/java/de/javengers/addressbook/service/AddressBookService.java
@@ -1,12 +1,26 @@
 package de.javengers.addressbook.service;
 
+import de.javengers.addressbook.db.AddressBookEntryRepository;
+import de.javengers.addressbook.db.CategoryRepository;
+import de.javengers.addressbook.db.PostalAddressRepository;
 import de.javengers.addressbook.model.AddressBookEntry;
 import de.javengers.addressbook.model.User;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressBookService {
+    private CategoryRepository categoryRepository;
+    private PostalAddressRepository postalAddressRepository;
+    private AddressBookEntryRepository addressBookEntryRepository;
+
+    @Autowired
+    public AddressBookService(CategoryRepository categoryRepository, PostalAddressRepository postalAddressRepository, AddressBookEntryRepository addressBookEntryRepository) {
+        this.categoryRepository = categoryRepository;
+        this.postalAddressRepository = postalAddressRepository;
+        this.addressBookEntryRepository = addressBookEntryRepository;
+    }
+
     public Long createAddressBookEntry(User user, AddressBookEntry entry) {
         return 1L;
     }
--- src/test/java/de/javengers/addressbook/service/AddressBookServiceTest.java
+++ src/test/java/de/javengers/addressbook/service/AddressBookServiceTest.java
@@ -1,23 +1,55 @@
 package de.javengers.addressbook.service;
 
+import de.javengers.addressbook.db.AddressBookEntryRepository;
+import de.javengers.addressbook.db.AddressBookRepository;
+import de.javengers.addressbook.db.CategoryRepository;
+import de.javengers.addressbook.db.PostalAddressRepository;
+import de.javengers.addressbook.model.AddressBook;
 import de.javengers.addressbook.model.AddressBookEntry;
 import de.javengers.addressbook.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class AddressBookServiceTest {
 
+    private CategoryRepository categoryRepository;
+    private PostalAddressRepository postalAddressRepository;
+    private AddressBookEntryRepository addressBookEntryRepository;
+    private AddressBookRepository addressBookRepository;
+
+    @BeforeEach
+    void setUp() {
+        categoryRepository = mock(CategoryRepository.class);
+        postalAddressRepository = mock(PostalAddressRepository.class);
+        addressBookEntryRepository = mock(AddressBookEntryRepository.class);
+        addressBookRepository = mock(AddressBookRepository.class);
+    }
 
     @Test
-    void testCreateAddressBook(){
-        AddressBookService service = new AddressBookService();
-        assertThat(service.createAddressBookEntry(new User(), new AddressBookEntry())).isEqualTo(1);
+    void testCreateAddressBook() {
+        AddressBookService service = new AddressBookService(categoryRepository, postalAddressRepository, addressBookEntryRepository);
 
-//        verify()
+        AddressBookEntry entry = new AddressBookEntry();
+        User user = new User();
+        user.setId(123L);
+
+        AddressBook newAddressBook = new AddressBook();
+        newAddressBook.setUser(user);
+        newAddressBook.setId(12L);
+
+        when(addressBookRepository.findByUser(user.getId())).thenReturn(List.of());
+        assertThat(service.createAddressBookEntry(user, entry)).isEqualTo(1);
+        verify(addressBookRepository).findByUser(user.getId());
+        verify(addressBookRepository.save(newAddressBook));
+        verify(categoryRepository).saveAll(entry.getCategories());
+        verify(postalAddressRepository).saveAll(entry.getPostalAddress());
+        verify(addressBookEntryRepository).save(entry);
+
     }
 
 }
